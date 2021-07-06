1015116798c1153696817377fa349499b54398b2
HuGe <1203747102@qq.com>
2021-07-06 10:40:04 +0800

    优化一些问题

--- app/src/main/java/com/weight/baseadapter/MainActivity.java
+++ app/src/main/java/com/weight/baseadapter/MainActivity.java
@@ -14,43 +14,55 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextAdapter mAdapter;
+    private List<String> mData;
+    private RecyclerView mRvData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView rvData = findViewById(R.id.rv_data);
-        rvData.setLayoutManager(new LinearLayoutManager(this));
-        List<String> data = new ArrayList<>();
+        mRvData = findViewById(R.id.rv_data);
+        mRvData.setLayoutManager(new LinearLayoutManager(this));
+        mData = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
-            data.add("胡歌：" + i);
+            mData.add("胡歌：" + i);
         }
-        TextAdapter adapter = new TextAdapter(this, data);
+        mAdapter = new TextAdapter(this, mData);
 
-        View headView = LayoutInflater.from(this).inflate(R.layout.item_text_view, rvData, false);
+        View headView = LayoutInflater.from(this).inflate(R.layout.item_text_view, mRvData, false);
         AppCompatTextView atvContent = headView.findViewById(R.id.atv_content);
         atvContent.setText("我的headView");
-        adapter.addHeadView(headView);
+        mAdapter.addHeadView(headView);
 
-        View footView = LayoutInflater.from(this).inflate(R.layout.item_text_view, rvData, false);
+        View footView = LayoutInflater.from(this).inflate(R.layout.item_text_view, mRvData, false);
         AppCompatTextView atvFootContent = footView.findViewById(R.id.atv_content);
         atvFootContent.setText("我的FootView");
-        adapter.addFootView(footView);
+        mAdapter.addFootView(footView);
 
-        rvData.setAdapter(adapter);
+        mRvData.setAdapter(mAdapter);
 
-        rvData.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                adapter.removeFootView();
-                View footView1 = getLayoutInflater().inflate(R.layout.item_text_view, rvData, false);
-                AppCompatTextView atvFootContent1 = footView1.findViewById(R.id.atv_content);
-                atvFootContent1.setText("我的FootView------");
-                adapter.addFootView(footView1);
+        mRvData.postDelayed(mRunnable, 3000);
 
-                data.clear();
-                adapter.notifyDataSetChanged();
-            }
-        }, 3000);
+    }
 
+    private final Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mAdapter.removeFootView();
+            View footView1 = getLayoutInflater().inflate(R.layout.item_text_view, mRvData, false);
+            AppCompatTextView atvFootContent1 = footView1.findViewById(R.id.atv_content);
+            atvFootContent1.setText("我的FootView------");
+            mAdapter.addFootView(footView1);
+
+            mData.clear();
+            mAdapter.notifyDataSetChanged();
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRvData.removeCallbacks(mRunnable);
     }
 }
